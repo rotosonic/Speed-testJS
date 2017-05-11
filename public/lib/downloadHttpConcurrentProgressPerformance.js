331e7057ec7f36b8af56bdfb52e3b126081cfec9
Richard Helferty <richard_helferty@comcast.com>
2017-05-11 15:54:07 -0400

    add files and update size

---
+++ public/lib/downloadHttpConcurrentProgressPerformance.js
@@ -0,0 +1,259 @@
+/*
+ * *
+ *  Copyright 2014 Comcast Cable Communications Management, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+
+(function () {
+    'use strict';
+    /**
+     * download testing based on httpRequests
+     * @param string server endpoint for upload testing
+     * @param string post or get request
+     * @param integer number of concurrentRuns
+     * @param integer timeout of the request
+     * @param integer length of the testLength
+     * @param integer when to calculate moving average
+     * @param function callback function for test suite complete event
+     * @param function callback function for test suite progress event
+     * @param function callback function for test suite abort event
+     * @param function callback function for test suite timeout event
+     * @param function callback function for test suite error event
+     **/
+    function downloadHttpConcurrentProgressPerformance(urls,  type, concurrentRuns, timeout, testLength, movingAverage, callbackComplete, callbackProgress, callbackAbort,
+                                            callbackTimeout, callbackError, size, progressIntervalDownload, monitorInterval) {
+        this.urls = urls;
+        this.size = size;
+        this.type = type;
+        this.concurrentRuns = concurrentRuns;
+        this.timeout = timeout;
+        this.testLength = testLength;
+        this.movingAverage = movingAverage;
+        //time to capture onProgressEvent
+        this.progressIntervalDownload = progressIntervalDownload;
+        //time for monitor to calcualte stats
+        this.monitorInterval = monitorInterval;
+        //unique id or test
+        this._testIndex = 0;
+        //array holding active tests
+        this._activeTests = [];
+        this.clientCallbackComplete = callbackComplete;
+        this.clientCallbackProgress = callbackProgress;
+        this.clientCallbackAbort = callbackAbort;
+        this.clientCallbackTimeout = callbackTimeout;
+        this.clientCallbackError = callbackError;
+        //start time of test suite
+        this._beginTime = Date.now();
+        //boolean on whether test  suite is running or not
+        this._running = true;
+        //array holding  results
+        this.finalResults = [];
+        //monitor interval
+        this.interval = null;
+        //total probe bytes
+        this.totalBytes = 0;
+        //results object array
+        this.results =[];
+        //results count
+        this.resultsCount = 0;
+        //results to send to client
+        this.downloadResults = [];
+    }
+
+    /**
+     * onError method
+     * @return error object
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.onTestError = function (result) {
+        if (this._running) {
+            this.clientCallbackError(result);
+            clearInterval(this.interval);
+            this._running = false;
+        }
+    };
+    /**
+     * onAbort method
+     * @return abort object
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.onTestAbort = function (result) {
+      this._storeResults(result);
+      this.totalBytes = this.totalBytes + result.loaded;
+    };
+    /**
+     * onTimeout method
+     * @return timeout object
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.onTestTimeout = function () {
+        if(this._running) {
+            if ((Date.now() - this._beginTime) > this.testLength) {
+                clearInterval(this.interval);
+                if (this.downloadResults && this.downloadResults.length) {
+                  this.clientCallbackComplete(this.downloadResults);
+                } else {
+                    this.clientCallbackError('no measurements obtained');
+                }
+                this._running = false;
+            }
+
+        }
+    };
+
+    /**
+     * onComplete method
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.onTestComplete = function (result) {
+
+        if (!this._running) {
+            return;
+        }
+
+        //store results
+        this._storeResults(result);
+        this.start();
+        };
+
+
+
+    /**
+     * onProgress method
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.onTestProgress = function (result) {
+        if (!this._running) {
+            return;
+        }
+        this.totalBytes = this.totalBytes + result.loaded;
+        this._storeResults(result);
+
+    };
+
+    /**
+     * Start the test
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.start = function () {
+      if (!this._running) {
+            return;
+      }
+
+            for (var g = 1; g <= this.concurrentRuns; g++) {
+                this._testIndex++;
+                var request = new window.xmlHttpRequestPerformance('GET', this.urls[g]+ this.size +  '&r=' + Math.random(), this.timeout, this.onTestComplete.bind(this), this.onTestProgress.bind(this),
+                    this.onTestAbort.bind(this), this.onTestTimeout.bind(this), this.onTestError.bind(this),this.progressIntervalDownload);
+                this._activeTests.push({
+                    xhr: request,
+                    testRun: this._testIndex
+                });
+                request.start(0, this._testIndex);
+            }
+
+    };
+
+    /**
+     * Cancel the test
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.abortAll = function () {
+        clearInterval(this.interval);
+        for (var i = 0; i < this._activeTests.length; i++) {
+            if (typeof(this._activeTests[i]) !== 'undefined') {
+                this._activeTests[i].xhr._request.abort();
+            }
+        }
+    };
+
+    /**
+     * store speedtest measurements
+     * @param result
+     * @private
+     */
+    downloadHttpConcurrentProgressPerformance.prototype._storeResults = function (result) {
+      this.results.push(result);
+    };
+
+
+    /**
+     * Monitor testSeries
+     */
+    downloadHttpConcurrentProgressPerformance.prototype._monitor = function () {
+        var intervalBandwidth = 0;
+        var totalLoaded = 0;
+        var totalTime = 0;
+        var intervalCounter = 0;
+        this.resultsCount++;
+
+        if (this.results.length > 0) {
+            for (var i = 0; i < this.results.length; i++) {
+
+                if (this.results[i].timeStamp > (performance.now() - (this.monitorInterval))) {
+                    intervalBandwidth = intervalBandwidth + parseFloat(this.results[i].bandwidth);
+                    totalLoaded = totalLoaded + this.results[i].chunckLoaded;
+                    totalTime = totalTime + this.results[i].totalTime;
+                    intervalCounter++;
+                }
+            }
+            if (!isNaN(intervalBandwidth / intervalCounter)) {
+                var transferSizeMbs = (totalLoaded * 8) / 1000000;
+                var transferDurationSeconds = (this.monitorInterval) / 1000;
+                this.finalResults.push(transferSizeMbs / transferDurationSeconds);
+                var lastElem = Math.min(this.finalResults.length, this.movingAverage);
+                if (lastElem > 0) {
+                    var singleMovingAverage = 0;
+                    for (var j = 1; j <= lastElem; j++) {
+                        if (isFinite(this.finalResults[this.finalResults.length - j])) {
+                            singleMovingAverage = singleMovingAverage + this.finalResults[this.finalResults.length - j];
+
+                        }
+                    }
+                    singleMovingAverage = singleMovingAverage / lastElem;
+                    if (singleMovingAverage > 0) {
+                        this.downloadResults.push(singleMovingAverage);
+                        this.clientCallbackProgress(singleMovingAverage);
+                    }
+                }
+
+            }
+
+        }
+        //check for end of test
+        if ((Date.now() - this._beginTime) > (this.testLength)) {
+            this._running = false;
+            clearInterval(this.interval);
+            if (this.downloadResults && this.downloadResults.length) {
+                this.clientCallbackComplete(this.downloadResults);
+            } else {
+                this.clientCallbackError('no measurements obtained');
+            }
+            this.abortAll();
+        }
+
+    };
+
+    /**
+     * reset test variables
+     */
+    downloadHttpConcurrentProgressPerformance.prototype.initiateTest = function(){
+        this._testIndex = 0;
+        this.finalResults.length=0;
+        this._running = true;
+        this.interval = null;
+        this.downloadResults.length = 0;
+        this.totalBytes = 0;
+        this.start();
+        var self = this;
+        this.interval = setInterval(function () {
+          self._monitor();
+        }, this.monitorInterval);
+    };
+
+    window.downloadHttpConcurrentProgressPerformance = downloadHttpConcurrentProgressPerformance;
+})();
---
+++ public/lib/xmlhttprequestperformance.js
@@ -0,0 +1,261 @@
+/*
+ * *
+ *  Copyright 2014 Comcast Cable Communications Management, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+
+(function() {
+  'use strict';
+  /**
+  * extend XMLHttpRequest
+  * @param string method post or get request
+  * @param stirng url address for request
+  * @param integer timeout timeout for request
+  * @param function callback for onloaded function
+  * @param function callback for onprogress function
+  */
+  function xmlHttpRequestPerformance(method, url, timeout, callbackComplete, callbackProgress,callbackAbort,
+  callbackTimeout,callbackError, progressIntervalDownload){
+    this.method = method;
+    this.url = url;
+    this.timeout = timeout;
+    this.startTime = null;
+    this.endTime = null;
+    this.bandwidth = null;
+    this.latency = 0;
+    this.id=null;
+    this.prevTime = 0;
+    this.prevLoad = 0;
+    this.progressCount = 0;
+    this.totalBytes = 0;
+    this.currentTime = 0;
+    this.progressIntervalDownload = progressIntervalDownload;
+    this.progressIntervalUpload = 50;
+    this.callbackComplete = callbackComplete;
+    this.callbackProgress = callbackProgress;
+    this.callbackAbort = callbackAbort;
+    this.callbackTimeout = callbackTimeout;
+    this.callbackError = callbackError;
+    this.requestTimeout = null;
+    this._request = null;
+  }
+
+  /**
+   * Initiate the request
+   */
+  xmlHttpRequestPerformance.prototype._initiateRequest = function(){
+
+      if (this._request === null ||
+       typeof this._request === 'undefined') {
+       this._request = new XMLHttpRequest();
+       // Handle lifecycle events on wrapped request
+       this._request.onloadstart = this._handleLoadstart.bind(this);
+       this._request.onload = this._handleLoad.bind(this);
+       this._request.onabort = this._handleAbort.bind(this);
+       this._request.timout = this._handleTimeout.bind(this);
+       this.requestTimeout= setTimeout(this._request.abort.bind(this._request), this.timeout);
+       this._request.onerror = this._handleError.bind(this);
+       this._request.onreadystatechange = this._handleOnReadyStateChange.bind(this);
+       if(this.method==='GET') {
+          this._request.onprogress = this._handleOnProgressDownload.bind(this);
+        }
+        else{
+          this._request.upload.onprogress = this._handleOnProgressUpload.bind(this);
+        }
+     }
+  };
+  /**
+  * Execute the request
+  */
+    xmlHttpRequestPerformance.prototype.start = function(size, id, payload) {
+      this._initiateRequest();
+      this.id = id;
+      this.transferSize = size;
+      this._request.open(this.method, this.url, true);
+      this._request.timeout = this.timeout;
+      if(this.method==='POST') {
+        this._request.send(payload);
+      }
+      else{
+        this._request.send(null);
+      }
+
+    };
+  /**
+  * Mark the start time of the request
+  */
+    xmlHttpRequestPerformance.prototype._handleLoadstart = function() {
+      this.startTime = performance.now();
+      this.prevTime = performance.now();
+    };
+  /**
+  * Handle eror event
+  */
+  xmlHttpRequestPerformance.prototype._handleError = function() {
+     var err = {
+       statusText: this._request.statusText,
+       status: this._request.status
+     };
+     this.callbackError(err);
+   };
+    /**
+      * Handle the timeout event on the wrapped request
+      */
+     xmlHttpRequestPerformance.prototype._handleTimeout = function(response) {
+       this.totalTime = this.endTime - this.startTime;
+       var transferSizeMbs = (response.loaded * 8) / 1000000;
+       var transferDurationSeconds = this.totalTime/1000;
+       //package results
+       var result = {};
+       result.latency = this.totalTime;
+       result.bandwidth = transferSizeMbs/transferDurationSeconds;
+       result.id = this.id;
+       this.callbackTimeout(result);
+  };
+    /**
+      * Handle the abort event on the wrapped request
+      */
+     xmlHttpRequestPerformance.prototype._handleAbort = function(response) {
+       clearTimeout(this.requestTimeout);
+       this.totalTime = performance.now() - this.startTime;
+       var transferSizeMbs = (response.loaded * 8) / 1000000;
+       var transferDurationSeconds = this.totalTime/1000;
+       //package results
+       var result = {};
+       result.timeStamp = performance.now();
+       result.chunckLoaded = response.loaded - this.prevLoad;
+       result.time = this.totalTime;
+       result.loaded = response.loaded;
+       result.bandwidth = transferSizeMbs/transferDurationSeconds;
+       result.id = this.id;
+       this.callbackAbort(result);
+
+  };
+  /**
+    * Close the request explicitly
+    */
+  xmlHttpRequestPerformance.prototype.close = function () {
+      this._request.abort();
+  };
+
+  /**
+   * Handle the load event on the wrapped request
+   */
+  xmlHttpRequestPerformance.prototype._handleOnReadyStateChange = function () {
+
+    if(this._request.readyState === 4 && this._request.status === 200) {
+              var result = {};
+              result.totalTime = performance.now() - this.startTime;
+              result.id = this.id;
+              if(this.method==='POST'){
+                var transferSizeMbs = (this.transferSize * 8) / 1000000;
+                var transferDurationSeconds = result.totalTime/1000;
+                result.bandwidth = transferSizeMbs/transferDurationSeconds;
+                if(isFinite(result.bandwidth)) {
+                    this.callbackComplete(result);
+                }
+                return;
+              }
+
+          }
+    if(this._request.status > 399){
+      var err = {
+        statusText: this._request.statusText,
+        status: this._request.status
+      };
+      this.callbackError(err);
+      return;
+    }
+  };
+
+  /**
+   * Handle the load event on the wrapped request
+   */
+  xmlHttpRequestPerformance.prototype._handleLoad = function (response) {
+      this.totalTime = performance.now() - this.startTime;
+      var result = {};
+      result.time = this.totalTime;
+      this.totalBytes += response.loaded;
+      var transferSizeMbs = response.loaded * 8 / 1000000;
+      var transferDurationSeconds = this.totalTime/1000;
+      result.bandwidth = transferSizeMbs / transferDurationSeconds;
+      result.loaded = response.loaded;
+      result.id = this.id;
+      if(isFinite(result.bandwidth)) {
+          if (this.method === 'GET') {
+              this.callbackComplete(result);
+          }
+      }
+  };
+
+  /**
+    * Handle onProgress
+    */
+   xmlHttpRequestPerformance.prototype._handleOnProgressDownload = function (response) {
+        //measure bandwidth after one progress event due to rampup
+        if (this.progressCount > 1) {
+          this.currentTime = performance.now();
+          var result = {};
+          result.id = this.id;
+          result.totalTime = this.currentTime - this.prevTime;
+          var transferSizeMbs = ((response.loaded - this.prevLoad) * 8) / 1000000;
+            var transferDurationSeconds = (result.totalTime) / 1000;
+            result.bandwidth = transferSizeMbs / transferDurationSeconds;
+            console.log(result.bandwidth);
+            result.loaded = response.loaded;
+            result.startTime = this.startTime;
+            result.timeStamp = performance.now();
+            result.chunckLoaded = response.loaded - this.prevLoad;
+            if (isFinite(result.bandwidth)) {
+              this.callbackProgress(result);
+              this.prevTime = this.currentTime;
+              this.prevLoad = response.loaded;
+            }
+        //  }
+        }
+      //increment onProgressEvent
+      this.progressCount++;
+   };
+
+   /**
+     * Handle onProgress
+     */
+   xmlHttpRequestPerformance.prototype._handleOnProgressUpload = function (response) {
+       //measure bandwidth after one progress event due to rampup
+       if (this.progressCount > 1 && response.lengthComputable) {
+         this.currentTime = performance.now();
+           var result = {};
+           result.id = this.id;
+           result.totalTime = this.currentTime - this.prevTime;
+           if (result.totalTime > this.progressIntervalUpload) {
+               var transferSizeMbs = ((response.loaded - this.prevLoad) * 8) / 1000000;
+               var transferDurationSeconds = result.totalTime / 1000;
+               result.bandwidth = transferSizeMbs / transferDurationSeconds;
+               if (isFinite(result.bandwidth)) {
+                   this.callbackProgress(result);
+                   this.prevTime = this.currentTime;
+                   this.prevLoad = response.loaded;
+               }
+           }
+       }
+       //increment onProgressEvent
+       this.progressCount++;
+
+
+   };
+
+window.xmlHttpRequestPerformance = xmlHttpRequestPerformance;
+
+  })();
